a4471f7c39ae7d94173934a78e6ded6840dc39c6
alinaand97 <alinaand_97@mail.ru>
2024-12-09 17:19:35 +1000

    Add files via upload

---
+++ Model13.py
@@ -0,0 +1,28 @@
+# Асинхронность на практике
+
+import asyncio
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    for i in range(1, 6):
+        await asyncio.sleep(1 / power)
+        print(f'Силач {name} поднял {i} шар.')
+    print(f'Силач {name} закончил соревнования.')
+
+
+async def start_tournament():
+    participants = [
+        ('Pasha', 3),
+        ('Denis', 4),
+        ('Apollon', 5)
+    ]
+
+    tasks = []
+    for name, power in participants:
+        tasks.append(start_strongman(name, power))
+
+    await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    asyncio.run(start_tournament())
